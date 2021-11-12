505db1ef8fc742a6d6b0ba55c1280cb854cfb843
Pavel Novák <novak.pavel@ssakhk.cz>
2021-11-12 08:19:06 +0100

    Add project files.

---
+++ WindowsFormsApp1/Models/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class Game
+    {
+        public Team Home { get; set; }
+        public Team Host { get; set; }
+        public GamePeriod Period { get; set; }
+        public TimeSpan CurrentTime { get; set; }
+        public TimeSpan DefaultTime { get; set; }
+        public string Title { get; set; }
+    }
+    public enum GamePeriod
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Overtime
+    }
+}
---
+++ WindowsFormsApp1/Services/ScoreTableService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Models;
+
+namespace WindowsFormsApp1.Services
+{
+    public class ScoreTableService
+    {
+        private Game _model;
+
+        public ScoreTableService()
+        {
+            new ScoreTableService()
+            {
+                _model = new Game()
+                {
+                    Period = GamePeriod.First,
+                    Home = new Team()
+                    {
+                        Name = "Home",
+                        Score = 0,
+                    },
+                    Host = new Team()
+                    {
+                        Name = "Host",
+                        Score = 0,
+                    },
+                    CurrentTime = new TimeSpan(0, 15, 0),
+                    DefaultTime = new TimeSpan(0, 15, 0),
+                    Title = String.Empty,
+                }
+            };
+        }
+    }
+}
